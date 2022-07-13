033ce74084c706df52b8d1165dee50330a247877
bart-Liu <bart.liu@cnosdb.com>
2022-07-13 18:32:21 +0800

    missing file

---
+++ server/ae/hack.go
@@ -0,0 +1,25 @@
+package ae
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+// no copy to change slice to string
+func String(b []byte) (s string) {
+	pbytes := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	pstring := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	pstring.Data = pbytes.Data
+	pstring.Len = pbytes.Len
+	return
+}
+
+// no copy to change string to slice
+func Slice(s string) (b []byte) {
+	pbytes := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	pstring := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	pbytes.Data = pstring.Data
+	pbytes.Len = pstring.Len
+	pbytes.Cap = pstring.Len
+	return
+}
